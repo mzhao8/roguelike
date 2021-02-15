0c55312b9f97c69b7b22e1b8b9843001a69c0139
mzhao8 <mzhao8@gmail.com>
2021-02-15 10:26:25 -0500

    added FOV, and new tile types

--- engine.py
+++ engine.py
@@ -4,6 +4,7 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 
 from entity import Entity
@@ -12,7 +13,7 @@ from input_handlers import EventHandler
 
 
 class Engine:
-    # takes 3 args: entities, event handler, and player
+    # takes 4 args: entities, event handler, game map, and player
     # entities is a set (of entities), which behaves kind of like a list that enforces uniqueness. That is, we can’t add an Entity to the set twice, whereas a list would allow that. In our case, having an entity in entities twice doesn’t make sense.
     # event handler is the same event_handler that we used in main.py. It will handle our events.
     # player is the player Entity. We have a separate reference to it outside of entities for ease of access. We’ll need to access player a lot more than a random entity in entities.
@@ -27,6 +28,7 @@ class Engine:
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
+        self.update_fov()
 
     # pass events through and iterate through them
     def handle_events(self, events: Iterable[Any]) -> None:
@@ -35,14 +37,32 @@ class Engine:
 
             if action is None:
                 continue
-            action.perform(self, self.player)
+            action.perform(
+                self, self.player
+            )  # passing Engine to the action, providing it context to do what we want
+
+            self.update_fov()  # Update the FOV before the players next action
+
+    def update_fov(self) -> None:
+        """
+        Recompute the visible area based on the players point of view.
+        """
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is 'visible' it hsould be added to 'explored
+        self.game_map.explored |= self.game_map.visible
 
     # handles drawing our screen. iterate through self.entities, and print them in their proper locations, present the context, and clear the console
     def render(self, console: Console, context: Context) -> None:
         # game map render method draws it onto the screen
         self.game_map.render(console)
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            # only print entities in FOV
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
---
+++ procgen.py
@@ -0,0 +1,138 @@
+from __future__ import annotations
+
+import random
+from typing import Iterator, List, Tuple, TYPE_CHECKING
+
+import tcod
+
+from game_map import GameMap
+import tile_types
+
+if TYPE_CHECKING:
+    from entity import Entity
+
+
+class RectangularRoom:
+    # x y coordinates take the top left corner and computes the bottom right corner based on the width and height variables
+    def __init__(self, x: int, y: int, width: int, height: int):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + width
+        self.y2 = y + height
+
+    # read only variable for the center coordinates of the rectangle
+    @property
+    def center(self) -> Tuple[int, int]:
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+
+        return center_x, center_y
+
+    # represents inner portion of our room
+    # the + 1 on the self creates the wall (lists are 0 indexed)
+    # carve out - everything is a wall right now
+    @property
+    def inner(self) -> Tuple[slice, slice]:
+        """Return the inner area of the room as a 2D array index."""
+        return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
+
+    def intersects(self, other: RectangularRoom) -> bool:
+        """Return True if this room overlaps with another Rectangular Room."""
+
+        return (
+            self.x1 <= other.x2
+            and self.x2 >= other.x1
+            and self.y1 <= other.y2
+            and self.y2 >= other.y1
+        )
+
+
+def tunnel_between(
+    start: Tuple[int, int], end: Tuple[int, int]
+) -> Iterator[Tuple[int, int]]:
+
+    """ Return an L-shaped tunnel between these two points. """
+
+    x1, y1 = start
+    x2, y2 = end
+
+    if random.random() < 0.5:  # 50% chance
+        # Move horizontally, then vertically.
+        corner_x, corner_y = x2, y1
+    else:
+        # Move vertically, then horizontally
+        corner_x, corner_y = x1, y2
+
+    # Generate the coordinates for this tunnel using Bresenham lines
+
+    # coordinates for the corner in the L
+    for x, y in tcod.los.bresenham((x1, y1), (corner_x, corner_y)).tolist():
+        yield x, y
+
+    # coordinates for the corner to the end of the list
+    for x, y in tcod.los.bresenham((corner_x, corner_y), (x2, y2)).tolist():
+        yield x, y
+
+
+def generate_dungeon(
+    max_rooms: int,
+    room_min_size: int,
+    room_max_size: int,
+    map_width: int,
+    map_height: int,
+    player: Entity,
+) -> GameMap:
+    """Generate a new dungeon map"""
+    dungeon = GameMap(map_width, map_height)
+
+    rooms: List[RectangularRoom] = []
+
+    for r in range(max_rooms):
+        room_width = random.randint(room_min_size, room_max_size)
+        room_height = random.randint(room_min_size, room_max_size)
+
+        x = random.randint(0, dungeon.width - room_width - 1)
+        y = random.randint(0, dungeon.height - room_height - 1)
+
+        # "RectangularRoom" class makes rectangles easier to work with
+        new_room = RectangularRoom(x, y, room_width, room_height)
+
+        # Run through the other rooms and see if they intersect with this one.
+        if any(new_room.intersects(other_room) for other_room in rooms):
+            continue  # This room intersects, so go to next attempt
+        # if no intersections, then room is valid
+
+        # Dig out this rooms inner area.
+        dungeon.tiles[new_room.inner] = tile_types.floor
+
+        if len(rooms) == 0:
+            # The first room, where the player starts.
+            player.x, player.y = new_room.center
+        else:  # All rooms after the first
+            # Dig out a tunnel between this room and the previous
+            for x, y in tunnel_between(rooms[-1].center, new_room.center):
+                dungeon.tiles[x, y] = tile_types.floor
+
+        # Finally, append the new room to the list
+        rooms.append(new_room)
+    return dungeon
+
+
+# OLD
+"""def generate_dungeon(
+    map_width, map_height
+) -> GameMap:  # Create a game map using RectangularRoom class
+
+    dungeon = GameMap(map_width, map_height)
+
+    room_1 = RectangularRoom(x=20, y=15, width=10, height=15)
+    room_2 = RectangularRoom(x=35, y=15, width=10, height=15)
+
+    dungeon.tiles[room_1.inner] = tile_types.floor
+    dungeon.tiles[room_2.inner] = tile_types.floor
+
+    for x, y in tunnel_between(room_2.center, room_1.center):
+        dungeon.tiles[x, y] = tile_types.floor
+
+    return dungeon
+"""
